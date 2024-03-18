955ef88fe8ba8f9ab82ab6fc1791444394edad74
sokq44 <sokq44@gmail.com>
2024-03-18 20:28:27 +0100

    Docker

---
+++ app/forja-roja/page.tsx
@@ -0,0 +1,10 @@
+import AppRouter from 'next/dist/client/components/app-router'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Forja Roja</div>
+  )
+}
+
+export default page
