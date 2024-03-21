9b2974944f1c89ea01c2812c05b153ec5db5f586
KoniczynSzef <koniczynszef@gmail.com>
2024-03-21 23:12:51 +0100

    fix: fixed some eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
 
 import { motion } from "framer-motion";
@@ -17,51 +17,66 @@ const companies = [
     {
         name: "Company",
         // logo: ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
     },
     {
         name: "Company",
         // logo: ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
     },
     {
         name: "Company",
         // logo: ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
     },
     {
         name: "Company",
         // logo: ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
     },
     {
         name: "Company",
         // logo: ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
-    }
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit enim commodi, saepe quia dicta cumque laudantium minima sequi neque? Consequuntur delectus, autem tempore in ducimus explicabo a minus excepturi.",
+    },
 ];
 
 export default function Home() {
-  return (
-    <Carousel className="w-full max-w-5xl mx-auto">
-      <CarouselContent>
-        {companies.map((item, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <motion.div className="p-1" initial={{y: 1000}} animate={{y: 0}} transition={{delay: index * 0.1}}>
-              <Card>
-                <CardHeader>
-                    <CardTitle>{`${item.name} ${index + 1}`}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>{item.description}</p>
-                </CardContent>
-              </Card>
-            </motion.div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  );
+    return (
+        <Carousel className="w-full max-w-5xl mx-auto">
+            <CarouselContent>
+                {companies.map((item, index) => (
+                    <CarouselItem
+                        key={index}
+                        className="md:basis-1/2 lg:basis-1/3"
+                    >
+                        <motion.div
+                            className="p-1"
+                            initial={{ y: 1000 }}
+                            animate={{ y: 0 }}
+                            transition={{ delay: index * 0.1 }}
+                        >
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{`${item.name} ${
+                                        index + 1
+                                    }`}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <p>{item.description}</p>
+                                </CardContent>
+                            </Card>
+                        </motion.div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    );
 }
