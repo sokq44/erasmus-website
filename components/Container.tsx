d4670c61cc3ef0dbc8128c4aef920e194f876c58
KoniczynSzef <koniczynszef@gmail.com>
2024-03-22 16:38:09 +0100

    style: added cn function to container

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,11 +1,16 @@
+import { cn } from "@/lib/utils";
 import React, { FC } from "react";
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
 }
 
 const Container: FC<Props> = (props) => {
-    return <section className="py-24 flex flex-col">{props.children}</section>;
+    return (
+        <section className={cn("py-24 flex flex-col", props.className)}>
+            {props.children}
+        </section>
+    );
 };
 
 export default Container;
