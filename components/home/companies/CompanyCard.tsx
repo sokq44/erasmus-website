431783bca9aeab0457d401887babd29cc09143a4
KoniczynSzef <koniczynszef@gmail.com>
2024-03-23 17:30:29 +0100

    style: changed animation to be only once

--- components/home/companies/CompanyCard.tsx
+++ components/home/companies/CompanyCard.tsx
@@ -17,7 +17,7 @@ interface Props {
 
 const CompanyCard: FC<Props> = (props) => {
     const ref = React.useRef<HTMLDivElement>(null);
-    const inView = useInView(ref, { margin: "150px 0px 0px 0px" });
+    const inView = useInView(ref, { margin: "150px 0px 0px 0px", once: true });
     return (
         <Link
             href={`/companies/${props.company.name}`}
