6c3d7a981da8fbf4f623877c7dd4fd9a2a88eb54
sokq44 <sokq44@gmail.com>
2024-03-23 16:44:30 +0100

    Fixed some error regarding vercel deployment again

--- components/home/companies/CompaniesWrapper.tsx
+++ components/home/companies/CompaniesWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 
 import { companies } from "@/assets/companies";
 import CompanyCard from "./CompanyCard";
--- components/home/testimonials/Testimonials.tsx
+++ components/home/testimonials/Testimonials.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
