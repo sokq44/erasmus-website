57a362f4edc2b14639cbce5a128c716a23d00792
sokq44 <sokq44@gmail.com>
2024-03-24 14:09:14 +0100

    chore: changed the structure of data handling regarding the testimonials segment

---
+++ assets/testimonials.ts
@@ -0,0 +1,54 @@
+import type { Testimonial } from "@/types/testimonial";
+
+export const testimonials: Testimonial[] = [
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+  {
+    name: "Name",
+    img: "https://github.com/shadcn.png",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere tempore a quidem aperiam impedit nihil quasi odit animi porro distinctio deserunt deleniti totam, fugiat dolorem incidunt saepe dolorum blanditiis!",
+  },
+];
---
+++ components/home/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface Props {
+    children: React.ReactNode
+}
+
+const Footer: FC<Props> = (props) => {
+    return <div>footer</div>
+}
+
+export default Footer
