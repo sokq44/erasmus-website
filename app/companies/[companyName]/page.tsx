eb56cc363e0af22cf88631120f2acc8e35c91313
sokq44 <sokq44@gmail.com>
2024-03-27 21:26:59 +0100

    feat: added dynamic system of displaying photos from practices

--- app/companies/[companyName]/page.tsx
+++ app/companies/[companyName]/page.tsx
@@ -7,14 +7,16 @@ import Container from "@/components/home/Container";
 
 import { companies } from "@/assets/companies";
 import CompanyDescription from "@/components/company/CompanyDescription";
-import CompanyPhotos from "@/components/company/CompanyPhotos";
+import CompanyPhotos from "@/components/company/CompanyPhotos"; 
+import { getFilesInDirectory } from "@/assets/get-images";
 
 interface Props {
   params: { companyName: string };
 }
 
-const page: FC<Props> = (props) => {
+const page: FC<Props> = async (props) => {
   const company = companies.find((e) => e.slug === props.params.companyName);
+  const filePaths = await getFilesInDirectory(company?.slug);
 
   if (!company) return notFound();
 
@@ -22,7 +24,7 @@ const page: FC<Props> = (props) => {
     <Container className="relative">
       <CompanyHeader className={company.textColor} company={company} />
       <CompanyDescription>{company.description}</CompanyDescription>
-      <CompanyPhotos company={company}></CompanyPhotos>
+      <CompanyPhotos company={company} filePaths={filePaths} ></CompanyPhotos>
     </Container>
   );
 };
--- components/company/CompanyPhotos.tsx
+++ components/company/CompanyPhotos.tsx
@@ -1,100 +1,56 @@
 "use client";
 
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 
 import { cn } from "@/lib/utils";
 
 import Container from "../home/Container";
 
 import type { Company } from "@/types/company";
+
 import Image from "next/image";
 
 interface Props {
   company: Company;
+  filePaths: string[];
 }
 
-type CompanyImage = {
-  id: number;
-  imageSrc: string;
-  name: string;
-};
-
-const images: CompanyImage[] = [
-  {
-    id: 1,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 2,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 3,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 4,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 5,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 6,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 7,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 8,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 9,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-  {
-    id: 10,
-    imageSrc: "/companies-images/forja-roja/mr-beast.png",
-    name: "1",
-  },
-];
-
 const CompanyPhotos: FC<Props> = (props) => {
-  const { company } = props;
+  const { company, filePaths } = props;
+
   return (
-    <Container className="mt-10">
+    <Container>
       <span className={cn("text-4xl font-bold mx-auto", company.textColor)}>
         Zdjęcia
       </span>
-      <div className="mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {images.map((image, _) => (
-            <CompanyImage key={image.id} image={image} />
-          ))}
-        </div>
+      <div className="mt-8 p-10 grid grid-cols-1 gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {filePaths.map((path, i) => (
+          <CompanyImage key={i} path={path} />
+        ))}
       </div>
     </Container>
   );
 };
 
-function CompanyImage({ image }: { image: CompanyImage }) {
+function CompanyImage({ path }: { path: string }) {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <a href="#" className="group">
-      <div className="aspect-1 w-full overflow-hidden rounded-lg bg-gray-200">
-        <img src={image.imageSrc} className="group-hover:opacity-75" />
+      <div className="aspect-w-2 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-7">
+        <Image
+          alt=""
+          src="/companies-images/forja-roja/mr-beast.png"
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            "duration-700 ease-in-out group-hover:opacity-75",
+            isLoading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
       </div>
     </a>
   );
