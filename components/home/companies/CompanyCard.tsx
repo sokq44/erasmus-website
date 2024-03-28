947948d165a5b05bf5b54ee5850a52cae92fc6fc
Koniczyn <koniczynszef@gmail.com>
2024-03-28 09:46:55 +0100

    style: added actual ring

--- components/home/companies/CompanyCard.tsx
+++ components/home/companies/CompanyCard.tsx
@@ -12,6 +12,7 @@ interface Props {
     backgroundGradient: {
         from: string;
         to: string;
+        ring: string; 
     };
 }
 
@@ -24,7 +25,7 @@ const CompanyCard: FC<Props> = (props) => {
             href={`/companies/${props.company.href}`}
             className={`group bg-transparent ${
                 props.index % 2 === 1 ? "lg:translate-y-16" : ""
-            } focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary focus-visible:ring-offset-4 transition duration-300 rounded-xl `}
+            } focus-visible:outline-none focus-visible:ring-4 focus-visible:${props.backgroundGradient.ring} focus-visible:ring-offset-4 transition duration-300 rounded-xl `}
         >
             <motion.div ref={ref} whileHover={{ y: -5 }}>
                 <motion.div
