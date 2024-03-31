abb939eb78791d882cfa3db070c88e03c7fbfbd4
KoniczynSzef <koniczynszef@gmail.com>
2024-03-31 21:10:05 +0200

    chore: removed unnecessary div

--- components/home/testimonials/TestimonialCard.tsx
+++ components/home/testimonials/TestimonialCard.tsx
@@ -18,35 +18,33 @@ const TestimonialCard: FC<Props> = (props) => {
     const { testimonial } = props;
 
     return (
-        <CarouselItem className="lg:basis-1/3 md:basis-1/2 drop-shadow-[1px_1px_1px_rgba(0,180,216,1)]">
-            <div className="p-1">
-                <Card>
-                    <CardHeader>
-                        <div className="flex items-stretch">
-                            <div>
-                                <CardTitle className="text-3xl font-semibold">
-                                    {testimonial.name}
-                                </CardTitle>
-                                <CardDescription className=" mt-1">
-                                    Technik {testimonial.profile}
-                                </CardDescription>
-                            </div>
-                            <Avatar className="ml-auto size-[30%] drop-shadow-[1px_1px_1px_rgba(0,180,216,1)]">
-                                <AvatarImage
-                                    src={testimonial.img}
-                                    alt="Some random desc"
-                                />
-                                <AvatarFallback>U</AvatarFallback>
-                            </Avatar>
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3 drop-shadow-[1px_1px_1px_rgba(0,180,216,1)]">
+            <Card>
+                <CardHeader>
+                    <div className="flex items-stretch">
+                        <div>
+                            <CardTitle className="text-3xl font-semibold">
+                                {testimonial.name}
+                            </CardTitle>
+                            <CardDescription className=" mt-1">
+                                Technik {testimonial.profile}
+                            </CardDescription>
                         </div>
-                    </CardHeader>
-                    <CardContent className="flex items-center">
-                        <blockquote className="relative before:absolute before:bg-secondary before:w-2 before:-left-4 ml-1 before:h-4/6 before:rounded-full italic">
-                            {testimonial.testimonial}
-                        </blockquote>
-                    </CardContent>
-                </Card>
-            </div>
+                        <Avatar className="ml-auto size-[30%] drop-shadow-[1px_1px_1px_rgba(0,180,216,1)]">
+                            <AvatarImage
+                                src={testimonial.img}
+                                alt="Some random desc"
+                            />
+                            <AvatarFallback>U</AvatarFallback>
+                        </Avatar>
+                    </div>
+                </CardHeader>
+                <CardContent className="flex items-center">
+                    <blockquote className="relative before:absolute before:bg-secondary before:w-2 before:-left-4 ml-1 before:h-4/6 before:rounded-full italic">
+                        {testimonial.testimonial}
+                    </blockquote>
+                </CardContent>
+            </Card>
         </CarouselItem>
     );
 };
