5479f350ccdea33902967395e9b92508ef3ac054
sokq44 <sokq44@gmail.com>
2024-04-06 20:27:42 +0200

    chore: added photos of students to the testimonials

--- assets/testimonials.ts
+++ assets/testimonials.ts
@@ -1,53 +1,46 @@
 import type { Testimonial } from "@/types/testimonial";
 
 export const testimonials: Testimonial[] = [
-    {
-        name: "Stasiek",
-        img: "/testimonials/s-g.jpg",
-        profile: "Programista",
-        testimonial:
-            "Udało mi się Zdobyć cenne doświadczenie zawodowe. Bardzo mi się również podobał fakt rozwijania wiedzy w innym kraju.",
-    },
-    {
-        name: "Maks",
-        img: "/testimonials/m-m.jpg",
-        profile: "Informatyk",
-        testimonial:
-            "Uważam, że Erasmus to świetna okazja do zdobycia cennego doświadczenia zawodowego i poznania wielu wartościowych ludzi.",
-    },
-    {
-        name: "Wojtek",
-        img: "/testimonials/w-a.jpg",
-        profile: "Informatyk",
-        testimonial:
-            "W porównaniu do polskich praktyk, te hiszpańskie okazały się o wiele cenniejsze w doświadczenia techniczne jak i te językowe.",
-    },
-    {
-        name: "Eryk",
-        img: "/testimonials/e-w.jpg",
-        profile: "Programista",
-        testimonial:
-            "Na praktykach poznałem nowe zagadnienia, które zgadzają się z moimi zainteresowaniami zawodowymi, oraz spotkałem świetnych pracodawców, którzy oświecili mi te kwestie.",
-    },
-    {
-        name: "Jakub",
-        img: "/testimonials/e-w.jpg",
-        profile: "Informatyk",
-        testimonial:
-            "Jako osoba po praktykach w serwisie uważam, że Telemalaga Samsung to perfekcyjna możliwość do doszlifowania swoich umiejętności. Wiele dała mi praktyka w obcym kraju pod względem doświadczenia oraz doskonalenia języka.",
-    },
-    {
-        name: "Wiktoria",
-        img: "/testimonials/e-w.jpg",
-        profile: "Programista",
-        testimonial:
-            "Staż zagraniczny pozwolił mi nie tylko na zdobycie cennych umiejętności zawodowych, ale również poznanie nowych osób i kultury.",
-    },
-    {
-        name: "Piotrek",
-        img: "/testimonials/e-w.jpg",
-        profile: "Programista",
-        testimonial:
-            "Dzięki praktykom w Hiszpanii jestem bogatszy o doświadczenie zawodowe i umiejętności, które chcę wykorzystać w przyszłości.",
-    },
+  {
+    name: "Stasiek",
+    img: "/testimonials/s-g.jpg",
+    profile: "Programista",
+    testimonial:
+      "Udało mi się Zdobyć cenne doświadczenie zawodowe. Bardzo mi się również podobał fakt rozwijania wiedzy w innym kraju.",
+  },
+  {
+    name: "Maks",
+    img: "/testimonials/m-m.jpg",
+    profile: "Informatyk",
+    testimonial:
+      "Uważam, że Erasmus to świetna okazja do zdobycia cennego doświadczenia zawodowego i poznania wielu wartościowych ludzi.",
+  },
+  {
+    name: "Wojtek",
+    img: "/testimonials/w-a.jpg",
+    profile: "Informatyk",
+    testimonial:
+      "W porównaniu do polskich praktyk, te hiszpańskie okazały się o wiele cenniejsze w doświadczenia techniczne jak i te językowe.",
+  },
+  {
+    name: "Eryk",
+    img: "/testimonials/e-w.jpg",
+    profile: "Programista",
+    testimonial:
+      "Na praktykach poznałem nowe zagadnienia, które zgadzają się z moimi zainteresowaniami zawodowymi, oraz spotkałem świetnych pracodawców, którzy oświecili mi te kwestie.",
+  },
+  {
+    name: "Wiktoria",
+    img: "/testimonials/w-w.png",
+    profile: "Programista",
+    testimonial:
+      "Staż zagraniczny pozwolił mi nie tylko na zdobycie cennych umiejętności zawodowych, ale również poznanie nowych osób i kultury.",
+  },
+  {
+    name: "Piotrek",
+    img: "/testimonials/p-k.jpeg",
+    profile: "Programista",
+    testimonial:
+      "Dzięki praktykom w Hiszpanii jestem bogatszy o doświadczenie zawodowe i umiejętności, które chcę wykorzystać w przyszłości.",
+  },
 ];
