d4d8c9aa2af6d0a758963a19605cca44b71f2487
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-06 18:26:23 +0200

    style: changed subpage title animation

--- components/company/CompanyHeader/CompanyHeader.tsx
+++ components/company/CompanyHeader/CompanyHeader.tsx
@@ -4,32 +4,19 @@ import { cn } from "@/lib/utils";
 
 import Container from "@/components/home/Container";
 
-import { TypewriterEffectSmooth } from "../../ui/typewritter-effect";
+// import { TypewriterEffectSmooth } from "../../ui/typewritter-effect";
 
 import { Company } from "@/types/company";
 import CompanyDescription from "./CompanyDescription";
+import AnimatedTitle from "./AnimatedTitle";
 
 interface Props {
     className?: string;
     company: Company;
 }
 
-const bgColors = {
-    "forja-roja": "bg-red-600",
-    "microbit-servicio-tecnico": "bg-green-600",
-    "samsung-electronics": "bg-blue-600",
-    euromind: "bg-orange-600",
-    "jc-valvuar-audio": "bg-black",
-};
-
 const CompanyHeader: FC<Props> = (props) => {
     const { className, company } = props;
-
-    const words = [
-        {
-            text: company.name,
-        },
-    ];
 
     return (
         <>
@@ -39,10 +26,7 @@ const CompanyHeader: FC<Props> = (props) => {
                     className
                 )}
             >
-                <TypewriterEffectSmooth
-                    words={words}
-                    cursorClassName={bgColors[company.slug]}
-                />
+                <AnimatedTitle company={company} />
             </Container>
             <CompanyDescription>{company.description}</CompanyDescription>
         </>
